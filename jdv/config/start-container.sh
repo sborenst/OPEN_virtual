3a396de58a9f0b3921bb14048dcaee2f934f4214
jbride <jbride@redhat.com>
2015-01-15 15:46:06 -0500

    new

---
+++ jdv/config/start-container.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+JBOSS_HOME=/opt/jboss/eap
+CONTAINER_CONFIG=/opt/OPEN-jdv/config
+START_LOG_FILE=/tmp/start-jdv.log
+
+DOCKER_IP=$(ip addr show eth0 | grep -E '^\s*inet' | grep -m1 global | awk '{ print $2 }' | sed 's|/.*||')
+
+echo -en "STARTING JDV CONTAINER\nDOCKER_IP = $DOCKER_IP\n" > $START_LOG_FILE
+
+JBOSS_COMMON_ARGS="-Djboss.bind.address=$DOCKER_IP -Djboss.bind.address.management=$DOCKER_IP "
+
+# customize size of JVM heap
+JAVA_OPTS="-Xms128m -Xmx1303m -XX:MaxPermSize=256m -Djava.net.preferIPv4Stack=true -Djboss.modules.system.pkgs=$JBOSS_MODULES_SYSTEM_PKGS -Djava.awt.headless=true -Djboss.modules.policy-permissions=true"
+export JAVA_OPTS
+$JBOSS_HOME/bin/standalone.sh --server-config=standalone.xml $JBOSS_COMMON_ARGS >> /tmp/start-bpms.log 2>&1
