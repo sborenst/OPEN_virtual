76431d55c8d3e9d9c750cc529e0a5835414c9ed8
jbride <jeff bride>
2015-02-01 12:53:54 -0700

    first draft at adding postgresql datasources

--- jdv/config/start-container.sh
+++ jdv/config/start-container.sh
@@ -10,7 +10,14 @@ echo -en "STARTING JDV CONTAINER\nDOCKER_IP = $DOCKER_IP\n" > $START_LOG_FILE
 
 JBOSS_COMMON_ARGS="-Djboss.bind.address=$DOCKER_IP -Djboss.bind.address.management=$DOCKER_IP "
 
+if [ "x$POSTGRESQL_PORT_5432_TCP_ADDR" != "x" ]; then
+    echo -en "POSTGRESQL_PORT_5432_TCP_ADDR not set !\n" >> $START_LOG_FILE
+elif
+    PGSQL_ARGUMENTS=" -Dpgsql.jdv.driver=postgresql -Dpgsql.jdv.user=jdv -Dpgsql.jdv.password=jdv"
+    PGSQL_ARGUMENTS=" $PGSQL_ARGUMENTS -Dpgsql.jdv.connection_url=jdbc:postgresql://$POSTGRESQL_PORT_5432_TCP_ADDR:5432/ "
+fi
+
 # customize size of JVM heap
 JAVA_OPTS="-Xms128m -Xmx1303m -XX:MaxPermSize=256m -Djava.net.preferIPv4Stack=true -Djboss.modules.system.pkgs=$JBOSS_MODULES_SYSTEM_PKGS -Djava.awt.headless=true -Djboss.modules.policy-permissions=true"
 export JAVA_OPTS
-$JBOSS_HOME/bin/standalone.sh --server-config=standalone.xml $JBOSS_COMMON_ARGS >> /tmp/start-bpms.log 2>&1
+$JBOSS_HOME/bin/standalone.sh --server-config=standalone.xml $JBOSS_COMMON_ARGS $PGSQL_ARGUMENTS >> $START_LOG_FILE 2>&1
