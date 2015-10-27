4cee0a69489f3480967cb7d85abfa87689eb9c28
root <root@inf00-mwl.opentlc.com>
2015-10-27 09:01:20 -0400

    BPM OSE templates and Dockerfiles now working in ravello environment

--- bpm/ose/bpms/config/start-container.sh
+++ bpm/ose/bpms/config/start-container.sh
@@ -4,26 +4,31 @@ DOCKER_IP=$(ip addr show eth0 | grep -E '^\s*inet' | grep -m1 global | awk '{ pr
 
 START_LOG_FILE=/tmp/start-bpms.log
 
-DATABASE_SERVICE_NAME_UC=$(echo $DATABASE_SERVICE_NAME | tr [a-z] [A-Z])
+echo -en "environment variables = \n" > $START_LOG_FILE
+env >> $START_LOG_FILE
 
-echo -en "STARTING BPMS ${JBOSS_BPMS_VERSION}\nDOCKER_IP = $DOCKER_IP\n" > $START_LOG_FILE
+echo -en "\nSTARTING BPMS ${JBOSS_BPMS_VERSION}\nDOCKER_IP = $DOCKER_IP\n" >> $START_LOG_FILE
 
-echo -en "DATABASE_SERVICE_NAME = $DATABASE_SERVICE_NAME_UC\n" >> $START_LOG_FILE
 
-DATABASE_SERVICE_NAME_SERVICE_HOST=$(eval "echo \$$(echo $DATABASE_SERVICE_NAME_UC)_SERVICE_HOST")
 
-DATABASE_SERVICE_NAME_SERVICE_PORT=$(eval "echo \$$(echo $DATABASE_SERVICE_NAME_UC)_SERVICE_PORT")
+# Given an input of "gpe-bpm-mysql", create an output of: GPE_BPM_MYSQL_SERVICE_HOST
+DATABASE_SERVICE_NAME_MODIFIED=`eval echo "$DATABASE_SERVICE_NAME" | sed s/-/_/g | awk '{print toupper($0)}'`
+echo -en "DATABASE_SERVICE_NAME_MODIFIED = $DATABASE_SERVICE_NAME_MODIFIED\n" >> $START_LOG_FILE
+DATABASE_SERVICE_NAME_SERVICE_HOST=$(eval "echo \$$(echo $DATABASE_SERVICE_NAME_MODIFIED)_SERVICE_HOST")
 
-echo -en "DATABASE_SERVICE_NAME_HOST = ${DATABASE_SERVICE_NAME_SERVICE_HOST}:${DATABASE_SERVICE_NAME_SERVICE_PORT}\n" >> $START_LOG_FILE
+# Given an input of "gpe-bpm-mysql", create an output of: GPE_BPM_MYSQL_SERVICE_PORT
+DATABASE_SERVICE_NAME_SERVICE_PORT=$(eval "echo \$$(echo $DATABASE_SERVICE_NAME_MODIFIED)_SERVICE_PORT")
+
+echo -en "DATABASE_SERVICE_NAME_SOCKET = ${DATABASE_SERVICE_NAME_SERVICE_HOST}:${DATABASE_SERVICE_NAME_SERVICE_PORT}\n" >> $START_LOG_FILE
 
 JBOSS_COMMON_ARGS="-Djboss.bind.address=$DOCKER_IP -Djboss.bind.address.management=$DOCKER_IP -Djboss.node.name=server-$DOCKER_IP"
 
 BPMS_COMMON_ARGS="-Dorg.uberfire.nio.git.daemon.host=$DOCKER_IP -Dorg.uberfire.nio.git.ssh.host=$DOCKER_IP"
 
-if [ "x$DATABASE_SERVICE_NAME_UC" != "x" ]; then
+if [ "x$DATABASE_SERVICE_NAME_MODIFIED" != "x" ]; then
     MYSQL_ARGUMENTS=" -Dmysql.host.ip=$DATABASE_SERVICE_NAME_SERVICE_HOST -Dmysql.host.port=$DATABASE_SERVICE_NAME_SERVICE_PORT"
 else
-    echo -en "DATABASE_SERVICE_NAME_UC not set !\n" >> $START_LOG_FILE
+    echo -en "DATABASE_SERVICE_NAME_MODIFIED not set !\n" >> $START_LOG_FILE
 fi
 
 # configuration
@@ -32,7 +37,6 @@ if [ ! -d "$JBOSS_BPMS_DATA/configuration" ]; then
   cp -r $JBOSS_HOME/standalone/configuration $JBOSS_BPMS_DATA 
 fi
 
-$JBOSS_HOME/bin/standalone.sh --server-config=standalone.xml -Djboss.server.config.dir=$JBOSS_BPMS_DATA/configuration $JBOSS_COMMON_ARGS $BPMS_COMMON_ARGS $MYSQL_ARGUMENTS 0<&- &>/dev/null
+echo  -en "\n\n" >> $START_LOG_FILE
 
-
-
+$JBOSS_HOME/bin/standalone.sh --server-config=standalone.xml -Djboss.server.config.dir=$JBOSS_BPMS_DATA/configuration $JBOSS_COMMON_ARGS $BPMS_COMMON_ARGS $MYSQL_ARGUMENTS 0<&- &>>/tmp/start-eap.log
