e5da368c6dad1b61b99ae07dd2c3031ef02dffdb
jbride <jbride2001@yahoo.com>
2015-12-04 18:45:27 -0700

    change to bpm start-container.sh

--- bpm/ose/bpms/config/start-container.sh
+++ bpm/ose/bpms/config/start-container.sh
@@ -2,13 +2,12 @@
 
 DOCKER_IP=$(ip addr show eth0 | grep -E '^\s*inet' | grep -m1 global | awk '{ print $2 }' | sed 's|/.*||')
 
-START_LOG_FILE=/tmp/start-bpms.log
+START_LOG_FILE=/tmp/start-container.log
 
 echo -en "environment variables = \n" > $START_LOG_FILE
 env >> $START_LOG_FILE
 
 echo -en "\nSTARTING BPMS ${JBOSS_BPMS_VERSION}\nDOCKER_IP = $DOCKER_IP\n" >> $START_LOG_FILE
-
 
 
 # Given an input of "gpe-bpm-mysql", create an output of: GPE_BPM_MYSQL_SERVICE_HOST
@@ -21,15 +20,23 @@ DATABASE_SERVICE_NAME_SERVICE_PORT=$(eval "echo \$$(echo $DATABASE_SERVICE_NAME_
 
 echo -en "DATABASE_SERVICE_NAME_SOCKET = ${DATABASE_SERVICE_NAME_SERVICE_HOST}:${DATABASE_SERVICE_NAME_SERVICE_PORT}\n" >> $START_LOG_FILE
 
-JBOSS_COMMON_ARGS="-Djboss.bind.address=$DOCKER_IP -Djboss.bind.address.management=$DOCKER_IP -Djboss.node.name=server-$DOCKER_IP"
-
-BPMS_COMMON_ARGS="-Dorg.uberfire.nio.git.daemon.host=$DOCKER_IP -Dorg.uberfire.nio.git.ssh.host=$DOCKER_IP"
-
-if [ "x$DATABASE_SERVICE_NAME_MODIFIED" != "x" ]; then
+if [ "x$DATABASE_SERVICE_NAME_SERVICE_HOST" != "x" ]; then
     MYSQL_ARGUMENTS=" -Dmysql.host.ip=$DATABASE_SERVICE_NAME_SERVICE_HOST -Dmysql.host.port=$DATABASE_SERVICE_NAME_SERVICE_PORT"
 else
-    echo -en "DATABASE_SERVICE_NAME_MODIFIED not set !\n" >> $START_LOG_FILE
+    echo -en "DATABASE_SERVICE_NAME_SERVICE_HOST not set !\n" >> $START_LOG_FILE
+
+    // Possible that this container may be used in a non OSE environment and linked to a database as follows:  --link=gpe-bpm-mysql:mysql 
+    if [  "x$MYSQL_PORT_3306_TCP_ADDR" != "x" ]; then
+        MYSQL_ARGUMENTS=" -Dmysql.host.ip=$MYSQL_PORT_3306_TCP_ADDR -Dmysql.host.port=3306
+    else
+        echo -en "MYSQL_HOST_PORT_3306_TCP_ADDR not set !\n" >> $START_LOG_FILE
+    fi
 fi
+
+
+JBOSS_COMMON_ARGS="-Djboss.bind.address=$DOCKER_IP -Djboss.bind.address.management=$DOCKER_IP -Djboss.node.name=server-$DOCKER_IP"
+BPMS_COMMON_ARGS="-Dorg.uberfire.nio.git.daemon.host=$DOCKER_IP -Dorg.uberfire.nio.git.ssh.host=$DOCKER_IP"
+
 
 # configuration
 
