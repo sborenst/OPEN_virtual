11e8d5b684445b71272c967309537cd90b0dd9ec
jbride <jbride2001@yahoo.com>
2016-02-18 07:44:05 -0500

    latest jenkins

--- devops/devops_using_ose/jenkins/oc_build_deploy.sh
+++ devops/devops_using_ose/jenkins/oc_build_deploy.sh
@@ -1,17 +1,41 @@
-# 1)  Authenticate oc client with OSE Master API
-if [ -z &quot;$AUTH_TOKEN&quot; ]; then
-  AUTH_TOKEN=`cat /var/run/secrets/kubernetes.io/serviceaccount/token`
-fi
+#!/bin/bash
 
-if [ -e /run/secrets/kubernetes.io/serviceaccount/ca.crt ]; then
-  alias oc=&quot;oc -n $PROJECT --token=$AUTH_TOKEN --server=$OPENSHIFT_API_URL --certificate-authority=/run/secrets/kubernetes.io/serviceaccount/ca.crt &quot;
+
+echo "Total number of arguments: $#"
+echo "Argument 1: $1"
+echo "Argument 2: $2"
+echo "Argument 3: $3"
+echo "Argument 4: $4"
+echo "Argument 5: $5"
+
+if [ -z "$1" ]; then
+    OPENSHIFT_API_URL=https://master00-mwl.opentlc.com:8443
 else
-  alias oc=&quot;oc -n $PROJECT --token=$AUTH_TOKEN --server=$OPENSHIFT_API_URL --insecure-skip-tls-verify &quot;
+    OPENSHIFT_API_URL=$1
 fi
+if [ -z "$2" ]; then
+    PROJECT=rh-jbride-redhat-com-devops-using-ose
+else
+    PROJECT=$2
+fi
+if [ -z "$3" ]; then
+    BUILD_CONFIG=webapp-qa
+else
+    BUILD_CONFIG=$3
+fi
+AUTH_TOKEN=`cat /var/run/secrets/kubernetes.io/serviceaccount/token`
+
+echo -en "oc_build_deploy.sh:  arguments:\n\tOPENSHIFT_API_URL = $OPENSHIFT_API_URL\n\tPROJECT = $PROJECT\n\tBUILD_CONFIG = $BUILD_CONFIG\n\tAUTH_TOKEN = $AUTH_TOKEN\n"
+
+alias oc="oc -n $PROJECT --token=$AUTH_TOKEN --server=$OPENSHIFT_API_URL --certificate-authority=/run/secrets/kubernetes.io/serviceaccount/ca.crt --v=10"
 
 # 2)  Kick off build
-echo &quot;Triggering new application build and deployment&quot;
+echo "Triggering new application build and deployment:"
 BUILD_ID=`oc start-build ${BUILD_CONFIG}`
+if [ $? != 0 ];
+then
+    exit 1;
+fi
 
 # 3) Stream the logs for the build that just started
 rc=1
@@ -26,28 +50,28 @@ done
 set -e
 
 # 4)  Check that build has succeeded
-echo &quot;Checking build result status&quot;
+echo "Checking build result status"
 rc=1
 count=0
 attempts=50
 while [ $rc -ne 0 -a $count -lt $attempts ]; do
-  status=`oc get build ${BUILD_ID} -t &apos;{{.status.phase}}&apos;`
-  if [[ $status == &quot;Failed&quot; || $status == &quot;Error&quot; || $status == &quot;Canceled&quot; ]]; then
-    echo &quot;Fail: Build completed with unsuccessful status: ${status}&quot;
+  status=`oc get build ${BUILD_ID} -t '{{.status.phase}}'`
+  if [[ $status == "Failed" || $status == "Error" || $status == "Canceled" ]]; then
+    echo "Fail: Build completed with unsuccessful status: ${status}"
     exit 1
   fi
 
-  if [ $status == &quot;Complete&quot; ]; then
-    echo &quot;Build completed successfully, will test deployment next&quot;
+  if [ $status == "Complete" ]; then
+    echo "Build completed successfully, will test deployment next"
     rc=0
   else
     count=$(($count+1))
-    echo &quot;Attempt $count/$attempts&quot;
+    echo "Attempt $count/$attempts"
     sleep 5
   fi
 done
 
 if [ $rc -ne 0 ]; then
-    echo &quot;Fail: Build did not complete in a reasonable period of time&quot;
+    echo "Fail: Build did not complete in a reasonable period of time"
     exit 1
 fi
